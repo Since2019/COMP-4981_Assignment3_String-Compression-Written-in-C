9093853cf708dc8948919e216345d8027b0704c0
Rickson2019 <judaozhong@gmail.com>
2020-11-01 09:36:28 -0800

    added ./build

---
+++ build/CMakeFiles/huffman-decompress.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/huffman-decompress.dir/huffman-decompress.c.o"
+  "huffman-decompress"
+  "huffman-decompress.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/huffman-decompress.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
