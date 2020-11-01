0fff2c9b74026d8cd3d40868e562c21e7666ecab
Rickson2019 <judaozhong@gmail.com>
2020-10-31 22:46:41 -0700

    I'm implementing this bit_handling.c

---
+++ bit_handling.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <fcntl.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+
+/* Now we are going to create a bit array
+ * the bit array
+ *
+ */
+uint8_t twelve_bits_output(){
+    uint16_t twelve_bit_tmp;
+
+    uint8_t eight_bit_tmp;
+
+
+}
